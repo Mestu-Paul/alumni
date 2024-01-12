d4f82184cda4a19852269eefd79b896f69708ae6
Mestu Paul <paulmestu@gmail.com>
2024-01-13 01:02:43 +0600

    add gallery

---
+++ user/frontend/gallery.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Home</title>
+  <?php include '../../common/headRef.php'; ?>
+</head>
+
+<body>
+  <?php include 'navbar.php'; ?>
+  <div class="mx-3 my-3" style="min-height: 100vh;">
+    <div class="text-center headLine">Gallery</div>
+    <?php include("../../common/galleryBase.php"); ?>
+  </div>
+  <?php include '../../common/footer.php'; ?>
+</body>
+
+</html>
