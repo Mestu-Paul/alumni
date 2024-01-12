887e8ea22f2116d3d5bd07cf5d8459d0914dd050
Mestu Paul <paulmestu@gmail.com>
2024-01-13 01:39:17 +0600

    add admin login

---
+++ admin/backend/login.php
@@ -0,0 +1,39 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    include_once '../../conn.php';
+    // Connect to the database.
+    $db = connect();
+
+    // Prepare the SQL query.
+    $sql = 'SELECT * FROM user WHERE email = :email AND password = :password';
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':password', $password);
+
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Close the database connection.
+    $db = null;
+    if(session_status() !== PHP_SESSION_ACTIVE)
+      session_start();
+    if(count($users) === 0) {
+        $message = 'Invalid Email or password';
+        $_SESSION['message'] = $message;
+        header("Location: ../frontend/login.php");
+    }
+    else if($users[0]['role']!=='admin'){
+      $_SESSION["message"] = 'Not permitted';
+      header("Location: ../frontend/login.php");
+    }
+    else{
+      $_SESSION["user_role"] = $users[0]['role'];
+      $_SESSION["user_email"] = $users[0]['email'];
+      header("Location: ../frontend/index.php");
+    }
+  }
+?>
---
+++ admin/frontend/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Login</title>
+  <?php include '../../common/headRef.php'; ?>
+</head>
+
+<body>
+  <!-- <?php include '../navbar.php'; ?> -->
+  <?php
+  if (isset($_SESSION['user_email'])) {
+    header("Location: index.php");
+  }
+  ?>
+  <div class="headLine">
+    <marquee>Welcome CCN Alumni Management System</marquee>
+  </div>
+  <div style="min-height:100vh">
+    <div class="card mx-auto mt-5 py-3 pb-5" style="width:24rem; box-shadow:0px 0px 10px 2px #919191;">
+      <h5 class="card-title text-center headLine">Admin Panel Login</h5>
+      <form action="../backend/login.php" method="post" class="col-10 col-md-8 col-lg-5 mx-auto mt-4" style="width:18rem;">
+          <div class="row mb-3">
+            <input type="email" placeholder="Email address : " class="form-control" name="email" id="Email" aria-describedby="emailHelp">
+          </div>
+          <div class="row mb-3">
+            <input type="password" placeholder="Password : " class="form-control" name="password" id="Password">
+          </div>
+          <div class="text-center mt-5 d-flex justify-content-between">
+            <a type="button" class="btn btn-danger1 mx-3" href="../../user/frontend/index.php">Cancel</a>
+            <button type="submit" class="btn btn-info1 mx-3">Login</button>
+          </div>
+        </form>
+    </div>
+  </div>
+  <?php include '../../user/frontend/footer.php'; ?>
+</body>
+
+</html>
